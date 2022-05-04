5f1c6f21131d586df85bb5a8c1234525bca44171
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-05-04 21:00:44 +0530

    fix: Change 'crossorigin' to 'crossOrigin' for react

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ function Document() {
 
         {/* Font - Inter */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&display=swap" rel="stylesheet" />
 
         {/* Description */}
