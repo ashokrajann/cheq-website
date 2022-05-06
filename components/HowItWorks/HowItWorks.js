018410e02137a05c44803e5db24ece35fa38905a
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-05-06 08:49:48 +0530

    fix: resolve merge conflict

--- components/HowItWorks/HowItWorks.js
+++ components/HowItWorks/HowItWorks.js
@@ -7,22 +7,23 @@ function HowItWorks() {
   return (
     <Wrapper>
       <h2 className={styles.sectionTitle}>How it works</h2>
-      {howItWorksData.map(card => (
-        <div 
-          className={styles.stepContainer}
-          key={card.stepNumber}
-        >
-          <h3 className={styles.stepNumber}>{card.stepNumber}</h3>
-          <p className={styles.stepDetails}>{card.stepDetails}</p>
-          <Image 
-            src={card.imageSrc}
-            alt={card.alt}
-            width={312}
-            height={220}
-            priority
-          />
-        </div>
-      ))}
+      <div className={styles.stepContainer}>
+        {howItWorksData.map(card => (
+          <div 
+            className={styles.stepCard}
+            key={card.stepNumber}
+          >
+            <h3 className={styles.stepNumber}>{card.stepNumber}</h3>
+            <p className={styles.stepDetails}>{card.stepDetails}</p>
+            <Image 
+              src={card.imageSrc}
+              alt={card.alt}
+              width={312}
+              height={220}
+            />
+          </div>
+        ))}
+      </div>
     </Wrapper>
   ) 
 }
