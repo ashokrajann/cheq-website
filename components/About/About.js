11c18d238aeec01a3c1cee6ac7a98bdac231ae23
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-05-10 18:25:46 +0530

    style: Update Mission statement and add Investor logos in 'About Us' section

--- components/About/About.js
+++ components/About/About.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Wrapper } from "../../components";
 import styles from "./About.module.css";
 
@@ -5,7 +6,33 @@ function About() {
   return (
     <Wrapper>
       <h2 className={styles.aboutTitle}>About us</h2>
-      <p className={styles.aboutDescription}>We are the next-gen fintech solution to empower Indians with a new-age instant credit experience with unmatched technology. Cheq works on UPI technology and you can repay us within 30 days of your bill cycle. Say NO to overcrowded bank statements with UPI transactions.¯</p>
+      <p className={styles.aboutDescription}>Our mission is to simplify banking and payments. We aim to provide affordable credit to everyone in an efficient and convenient manner. <br />Some of the top venture firms share our mission.</p>
+      <div className={styles.investorLogos}>
+        <a 
+          href="https://www.bettercapital.vc/" 
+          target="_blank"
+          className={styles.investorLogo}
+        >
+          <Image
+            src="/images/better_logo.svg"
+            alt={"Better captial logo"}
+            width={120}
+            height={56}
+          />
+        </a>
+        <a 
+          href="https://www.bettercapital.vc/" 
+          target="_blank"
+          className={styles.investorLogo}
+        >
+          <Image
+            src="/images/prophetic_logo.svg"
+            alt={"Prophetic ventures logo"}
+            width={56}
+            height={56}
+          />
+        </a>
+      </div>
     </Wrapper>
   )
 }
