7d06309c4a24e4d9955a359467fdc8356581e891
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-05-10 19:43:04 +0530

    fix: Remove interactive links from investor logos

--- components/About/About.js
+++ components/About/About.js
@@ -8,30 +8,22 @@ function About() {
       <h2 className={styles.aboutTitle}>About us</h2>
       <p className={styles.aboutDescription}>Our mission is to simplify banking and payments. We aim to provide affordable credit to everyone in an efficient and convenient manner. <br />Some of the top venture firms share our mission.</p>
       <div className={styles.investorLogos}>
-        <a 
-          href="https://www.bettercapital.vc/" 
-          target="_blank"
-          className={styles.investorLogo}
-        >
+        <span className={styles.investorLogo}>
           <Image
             src="/images/better_logo.svg"
             alt={"Better captial logo"}
             width={120}
             height={56}
           />
-        </a>
-        <a 
-          href="https://www.bettercapital.vc/" 
-          target="_blank"
-          className={styles.investorLogo}
-        >
+        </span>
+        <span className={styles.investorLogo}>
           <Image
             src="/images/prophetic_logo.svg"
             alt={"Prophetic ventures logo"}
             width={56}
             height={56}
           />
-        </a>
+        </span>
       </div>
     </Wrapper>
   )
